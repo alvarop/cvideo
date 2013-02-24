ff5465c5c9ffb8016ffe9a4d1d18e3de64d9180e
Alvaro Prieto <apg88zx@gmail.com>
2013-02-23 19:25:21 -0800

    Testing for LPC1769

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 //
-// Basic LED-blink for the MBED.
+// Component video test
 //
 #include "LPC17xx.h"
 #include <stdint.h>
 
-#define LED4_PIN (23)
+#define LED2_PIN (22)
 
 int main() {
   
   SystemInit();
   
     // Setup P1.23 as output
-  LPC_GPIO1->FIODIR |= (1 << LED4_PIN);
+  LPC_GPIO0->FIODIR |= (1 << LED2_PIN);
     
   for(;;) {
     for(uint32_t delay = 0; delay < 10000000; delay++) {
@@ -19,13 +19,13 @@ int main() {
     }
     
     // Turn LED ON
-    LPC_GPIO1->FIOSET = (1 << LED4_PIN);
+    LPC_GPIO0->FIOSET = (1 << LED2_PIN);
     
     for(uint32_t delay = 0; delay < 10000000; delay++) {
        __asm("NOP");
     }
     
     // Turn LED OFF
-    LPC_GPIO1->FIOCLR = (1 << LED4_PIN);
+    LPC_GPIO0->FIOCLR = (1 << LED2_PIN);
   }
 }

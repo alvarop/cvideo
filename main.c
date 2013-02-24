c89379fe21417c2dde8f3e9953c2a1aa44b7c6b2
Alvaro Prieto <apg88zx@gmail.com>
2013-02-23 20:01:54 -0800

    Setup systick and test

--- main.c
+++ main.c
@@ -5,27 +5,38 @@
 #include <stdint.h>
 
 #define LED2_PIN (22)
+#define VIDEO_PIN (0) // P2.0
+
+volatile uint32_t systick_counter = 0;
+
+void SysTick_Handler (void)
+{
+	systick_counter++;
+}
 
 int main() {
   
   SystemInit();
-  
-    // Setup P1.23 as output
+
+  SysTick_Config(SystemCoreClock/1000 - 1); // Generate interrupt each 1 ms
+
+  // Setup P1.23 as output
   LPC_GPIO0->FIODIR |= (1 << LED2_PIN);
+  
+  // Setup video pin as output
+  LPC_GPIO2->FIODIR |= (1 << VIDEO_PIN);
     
   for(;;) {
-    for(uint32_t delay = 0; delay < 10000000; delay++) {
-       __asm("NOP");
+
+    if(0 == (systick_counter % 500)) {
+      // Toggle LED
+      LPC_GPIO0->FIOPIN ^= (1 << LED2_PIN);
     }
     
-    // Turn LED ON
-    LPC_GPIO0->FIOSET = (1 << LED2_PIN);
+    // Toggle pin to check how often systick we wake up (should be 1ms)
+    LPC_GPIO2->FIOPIN ^= (1 << VIDEO_PIN);
     
-    for(uint32_t delay = 0; delay < 10000000; delay++) {
-       __asm("NOP");
-    }
-    
-    // Turn LED OFF
-    LPC_GPIO0->FIOCLR = (1 << LED2_PIN);
+    __WFI(); // Sleep until next systick
+       
   }
 }
